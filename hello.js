f31eed6c95401115ff3165d1d5d7d3ed22fae690
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2022-01-12 07:47:47 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -141,7 +141,7 @@ function init( error, success ) {
        <br>
        <br>
        
-       <a href="#" onclick="signup_render();" class="btn btn-primary stretched-link" >Sign up!</a>
+       <a href="#" onclick="signup_render();" class="btn btn-primary btn-lg" >Sign up!</a>
        <br>
 
        ${error}
